71e33a7bf09b21b96cc4a54b8b343473ef2959bc
shunsuke-tamura <wwwxx4869@gmail.com>
2023-08-26 22:37:26 +0900

    update: pipev化してハロワ

--- api/main.py
+++ api/main.py
@@ -3,10 +3,12 @@ from api.routers import score, ranking
 
 app = FastAPI()
 
-# @app.get("/hello")
-# async def hello():
-#     return {"messae": "hello world!"}
 
-app = FastAPI()
+@app.get("/hello")
+async def hello():
+    return {"messae": "hello world!"}
+
+
+# app = FastAPI()
 app.include_router(ranking.router)
 app.include_router(score.router)
